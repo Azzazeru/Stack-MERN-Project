b9fa3402f8f901f2ae4c36e6b259bfc826084172
Azzazeru <aaron.fuentesdioca@gmail.com>
2024-07-03 00:12:52 -0400

    Env update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { API_URL } from '../config';
+
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: API_URL,
     withCredentials: true,
 })
 
